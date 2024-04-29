4708a043f3eb90e87804f5019e7358f5a51d160e
oraz <hudayberdiyevoraz@gmail.com>
2024-04-29 09:34:13 +0200

    ticketing project

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,12 +1,12 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
@@ -27,5 +27,20 @@ public class ProjectController {
         model.addAttribute("managers", userService.findAll());
         model.addAttribute("projects",projectService.findAll());
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+
+
+        projectService.save(project);
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectCode}")
+    public String deleteProject(@PathVariable("projectCode") String projectCode){
+
+        projectService.deleteById(projectCode);
+        return "redirect:/project/create";
     }
 }
