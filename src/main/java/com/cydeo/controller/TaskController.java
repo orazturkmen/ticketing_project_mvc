b3b561b73bb1068789e67c8e5dd4a2421553be63
oraz <hudayberdiyevoraz@gmail.com>
2024-04-30 02:31:05 +0200

    ticketing project task create

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,18 @@
+package com.cydeo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    @GetMapping("/create")
+    public String createTAsk(){
+
+
+        return "/task/create";
+    }
+}
