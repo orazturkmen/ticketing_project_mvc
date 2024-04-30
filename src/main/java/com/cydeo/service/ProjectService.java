bb9b7a9f7e6f515133193fd31debb0928e36da60
oraz <hudayberdiyevoraz@gmail.com>
2024-04-30 02:30:18 +0200

    ticketing project create

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -4,4 +4,5 @@ import com.cydeo.controller.ProjectController;
 import com.cydeo.dto.ProjectDTO;
 
 public interface ProjectService extends CrudService<ProjectDTO,String>{
+    void complete(ProjectDTO project);
 }
--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -35,6 +35,14 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> im
 
     @Override
     public void update(ProjectDTO object) {
+        if (object.getProjectStatus()==null){
+            object.setProjectStatus(findById(object.getProjectCode()).getProjectStatus());
+        }
         super.update(object.getProjectCode(), object);
+    }
+
+    @Override
+    public void complete(ProjectDTO project) {
+        project.setProjectStatus(Status.COMPLETE);
     }
 }
