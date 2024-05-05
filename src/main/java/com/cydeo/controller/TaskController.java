8db4af0922117bc90560d0f2e6027463e0502cf1
oraz <hudayberdiyevoraz@gmail.com>
2024-05-05 14:15:55 +0200

    ticketing project task create completed

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -46,4 +46,22 @@ public class TaskController {
         taskService.deleteById(id);
         return "redirect:/task/create";
     }
+
+    @GetMapping("/update/{id}")
+    public String editTask(@PathVariable("id") Long id, Model model){
+
+        model.addAttribute("task", taskService.findById(id));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
+
+        return "/task/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateTask(/*@PathVariable("id") Long id,*/ TaskDTO task){  //if field name(id) matches with object's variables name (Long id in the TaskDTO) does not need @PathVariable. Spring already knows.
+//        task.setId(id);
+        taskService.update(task);
+        return "redirect:/task/create";
+    }
 }
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -43,6 +43,12 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
 
     @Override
     public void update(TaskDTO task) {
+        if (task.getTaskStatus() == null){
+            task.setTaskStatus(Status.OPEN);
+        }
+        if (task.getAssignedDate() == null){
+            task.setAssignedDate(LocalDate.now());
+        }
         super.update(task.getId(),task);
     }
 }
